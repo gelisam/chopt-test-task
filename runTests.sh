8fed598c879bbeeec4969f432c24b14217850c6c
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-07 06:45:16 -0400

    fix test to use --address

--- runTests.sh
+++ runTests.sh
@@ -10,11 +10,8 @@ clear
 # argument-parsing logic here, we use a special role which tests the arguments.
 chopt-test-task check-args "$@"
 
-#cat nodelist.txt | while IFS='' read -r NODE_CONFIG; do
-#  # NODE_CONFIG looks like "--role=Slave --host=localhost --port=8080",
-#  # we want it to expand to multiple arguments
-#  chopt-test-task run-node "$@" $NODE_CONFIG &
-#done
-chopt-test-task run-node --role=Slave  --host=localhost --port=8083 "$@" &
-sleep 5
-chopt-test-task run-node --role=Master --host=localhost --port=8084 "$@" &
+cat nodelist.txt | while IFS='' read -r NODE_CONFIG; do
+  # NODE_CONFIG looks like "--role=Slave --address=localhost:8080:0",
+  # we want it to expand to multiple arguments
+  chopt-test-task run-node "$@" $NODE_CONFIG &
+done
