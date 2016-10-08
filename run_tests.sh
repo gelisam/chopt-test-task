a76ee48fafcacc369b0d47031376829d4b4cbca2
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-08 10:05:30 -0400

    fix: nodes weren't killed between each test
    When a unix process terminates, its children's parent id no longer
    points to that dead parent, so kill_all_children failed to kill them.
    
    The reason their parent terminated early was two-fold: first, I was
    iterating over nodelist.txt by piping it into a while loop, thereby
    causing bash to spawn a sub-process. After the while loop was over, the
    nodes spawed within that while loop lost their parent id. But even after
    replacing the while loop with a for loop, run_tests.sh itself
    terminated, so I also added a final while loop to make sure run_tests.sh
    hangs until run_tests_continuously.sh kills it and its children.
    
    Note that run_tests.sh doesn't kill the nodes after running the test, so
    it is only meant to be used as a helper by run_tests_continuously.sh, it
    is not meant to be used by the user to run the tests once.
    Hmm, maybe I should fix that?

--- run_tests.sh
+++ run_tests.sh
@@ -10,8 +10,16 @@ clear
 # argument-parsing logic here, we use a special role which tests the arguments.
 chopt-test-task check-args "$@"
 
-cat nodelist.txt | while IFS='' read -r NODE_CONFIG; do
+# temporarily replacing spaces by semicolons so we iterate on lines, not words
+for NODE_SEMICONFIG in $(cat nodelist.txt | tr ' ' ';'); do
+  NODE_CONFIG="$(echo "$NODE_SEMICONFIG" | tr ';' ' ')"
+  
   # NODE_CONFIG looks like "--role=Slave --address=localhost:8080:0",
   # we want it to expand to multiple arguments
   chopt-test-task run-node "$@" $NODE_CONFIG &
+done
+
+# wait for CTRL-C
+while true; do
+  sleep 100
 done
