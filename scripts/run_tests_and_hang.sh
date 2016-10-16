164f6f8294fcdc1c2ae14f52c02ceef30fdfcb86
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-16 12:04:37 -0400

    rename nodelist.txt to node_list.txt
    for consistency with the other non-code files which all use snake_case

--- scripts/run_tests_and_hang.sh
+++ scripts/run_tests_and_hang.sh
@@ -2,7 +2,7 @@
 set -e
 
 # temporarily replacing spaces by semicolons so we iterate on lines, not words
-for SEMI_ADDRESS in $(cat nodelist.txt | tr ' ' ';'); do
+for SEMI_ADDRESS in $(cat node_list.txt | tr ' ' ';'); do
   ADDRESS="$(echo "$SEMI_ADDRESS" | tr ';' ' ')"
   
   echo chopt-test-task run-node "$@" --address="$ADDRESS"
@@ -11,6 +11,6 @@ done
 
 
 # wait for all the child processes to terminate
-for SEMI_ADDRESS in $(cat nodelist.txt | tr ' ' ';'); do
+for SEMI_ADDRESS in $(cat node_list.txt | tr ' ' ';'); do
   wait
 done
--- src/Main.hs
+++ src/Main.hs
@@ -24,10 +24,10 @@ main = do
         -- if the arguments were incorrect, 'execParser' would have complained.
         return ()
       RunNode userConfig (FileProvidedConfig myAddress) -> do
-        allAddresses <- join $ mapM parse <$> lines <$> readFile "nodelist.txt"
+        allAddresses <- join $ mapM parse <$> lines <$> readFile "node_list.txt"
         myIndex <- case elemIndex myAddress allAddresses of
           Just x -> return x
-          Nothing -> fail $ printf "%s is a valid address but it is not listed in nodelist.txt" (show myAddress)
+          Nothing -> fail $ printf "%s is a valid address but it is not listed in node_list.txt" (show myAddress)
         
         let nbNodes       = length allAddresses
         let peerAddresses = filter (/= myAddress) allAddresses
