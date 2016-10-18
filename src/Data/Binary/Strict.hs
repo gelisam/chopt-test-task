888a6fd2634f6f9e19f139ca751f26adfb9a5c16
Samuel Gélineau <gelisam+github@gmail.com>
2016-10-18 08:49:02 -0400

    trace bytes sent

--- src/Data/Binary/Strict.hs
+++ src/Data/Binary/Strict.hs
@@ -8,13 +8,16 @@ import qualified Data.Binary as Lazy
 import           Data.Binary (Binary)
 import qualified Data.ByteString as Strict
 import qualified Data.ByteString.Lazy as Lazy
+import Debug.Trace
+
 
 
 encode :: Binary a => a -> Strict.ByteString
-encode = Lazy.toStrict . Lazy.encode
+encode x = let strictString = Lazy.toStrict $ Lazy.encode x
+            in trace (show strictString) strictString
 
 decode :: Binary a => Strict.ByteString -> a
 decode strictString = let lazyString = Lazy.fromStrict strictString
                        in case Lazy.decodeOrFail lazyString of
-                            Left err -> error (show err)
+                            Left err -> error (show (strictString, err))
                             Right (_,_,x) -> x
